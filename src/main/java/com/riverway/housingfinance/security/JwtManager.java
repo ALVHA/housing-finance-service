dc872212952ce1e96d3b1f69f238d0be4249cfbe
KangGilHwan <rlfghksop@naver.com>
2019-04-06 22:52:10 +0900

    feat: Add JwtTokenInterceptor, it check the validity of the token and refresh it if necessary

--- src/main/java/com/riverway/housingfinance/security/JwtManager.java
+++ src/main/java/com/riverway/housingfinance/security/JwtManager.java
@@ -3,13 +3,11 @@ package com.riverway.housingfinance.security;
 import com.riverway.housingfinance.support.exception.CannotGenrateJwtKeyException;
 import com.riverway.housingfinance.support.exception.ErrorMessage;
 import com.riverway.housingfinance.support.exception.JwtAuthorizationException;
-import com.riverway.housingfinance.user.UserDto;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jws;
 import io.jsonwebtoken.Jwts;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Service;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
 
@@ -20,23 +18,23 @@ import java.util.Date;
 import static io.jsonwebtoken.SignatureAlgorithm.HS256;
 
 @Slf4j
-@Component("jwtManager")
+@Component
 public class JwtManager {
 
     private static final String ISSUER = "riverway.com";
     private static final String SALT = "Sea is salt";
-    private static final String CLAIM_KEY_USER_ID = "userId";
+    public static final String CLAIM_KEY_USER_ID = "userId";
 
     private static final int HOUR = 60 * 60 * 1000;
     private final int expirationTime = 2 * HOUR;
 
-    public String createToken(UserDto user) {
+    public String createToken(String userId) {
         return Jwts.builder()
                 .setHeaderParam("typ", "JWT")
                 .setHeaderParam("alg", HS256)
                 .setIssuer(ISSUER)
                 .setExpiration(new Date(System.currentTimeMillis() + expirationTime))
-                .claim(CLAIM_KEY_USER_ID, user.getUserId())
+                .claim(CLAIM_KEY_USER_ID, userId)
                 .signWith(HS256, generateKey())
                 .compact();
     }
@@ -50,8 +48,7 @@ public class JwtManager {
         }
     }
 
-    public String decode() {
-        HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.currentRequestAttributes()).getRequest();
+    public String decode(HttpServletRequest request) {
         String jwt = request.getHeader("Authorization");
         try {
             Jws<Claims> claims = parse(jwt);
@@ -61,9 +58,26 @@ public class JwtManager {
         }
     }
 
+    public String decode() {
+        HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.currentRequestAttributes()).getRequest();
+        return decode(request);
+    }
+
     public Jws<Claims> parse(String jwt) {
         return Jwts.parser()
                 .setSigningKey(generateKey())
                 .parseClaimsJws(jwt);
+    }
+
+    public String refresh(String jwt) {
+        String userId = parseUserId(jwt);
+        return createToken(userId);
+    }
+
+    public String parseUserId(String jwt) {
+        return parse(jwt)
+                .getBody()
+                .get(CLAIM_KEY_USER_ID)
+                .toString();
     }
 }
--- src/main/java/com/riverway/housingfinance/support/exception/ErrorMessage.java
+++ src/main/java/com/riverway/housingfinance/support/exception/ErrorMessage.java
@@ -6,6 +6,8 @@ public class ErrorMessage {
     public static final String READ_FAILED = "파일을 읽는데 실패하였습니다.";
     public static final String WRONG_PASSWORD = "비밀번호가 틀렸습니다.";
     public static final String NOT_MATCH_BANK = "해당 은행이 없습니다.";
+
     public static final String GENERATE_JWT_KEY = "JWT key를 만들 수 없습니다.";
     public static final String INVALID_TOKEN = "토큰이 유효하지 않습니다.";
+    public static final String NOT_EXIST_TOKEN = "토큰이 존재하지 않습니다.";
 }
